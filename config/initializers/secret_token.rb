7c4e2e3855848152ab8559be75682d0b595865d8
Alex Howington <alex.howington@gmail.com>
2013-11-23 20:42:00 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LolSandbox::Application.config.secret_token = 'e32f791fb389967554551cac611d2c979017804e4b9b44cf07655da496c41cab7fd881895bbe8c33e0e8511db9ef8440497bc48f764de882f91313a30ec53d01'
